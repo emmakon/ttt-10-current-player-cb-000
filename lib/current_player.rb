c9a3b15e1737f72cc9d3660d9106a900213238a6
Emma Konrad <emma.konrad92@gmail.com>
2018-08-16 18:35:40 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+  if turn == "X" || turn == "O" 
+    counter += 1 
+  else
+    return counter
+  end
+ end
+end
+
+def current_player
+end
